a4101cdfbb5f5b8616e0547ca472f5ea05129ea9
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-10-09 17:19:55 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*i
+    return f
+num=int(input("Enter an integer number"))
+#calling function
+print("The factorial of",num,"is",factorial(num))
