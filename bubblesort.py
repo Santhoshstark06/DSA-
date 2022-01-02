1b743052e7f4b0f3fe5d7d0d57b887d0ee7011ad
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2022-01-02 16:45:46 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,4 +1,6 @@
 
+
+
 def bubble_sort(list1):  
     for i in range(0,len(list1)-1):  
         for j in range(len(list1)-1):  
@@ -6,7 +8,8 @@ def bubble_sort(list1):
                 temp = list1[j]  
                 list1[j] = list1[j+1]  
                 list1[j+1] = temp  
-                
+         
+        
     return list1  
   
 list1 = [5, 3, 8, 6, 7, 2]  
