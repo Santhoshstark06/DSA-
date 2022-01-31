36c88c97a1e09947d64973dede40d40527a80f8f
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2022-01-31 20:20:46 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,3 +1,4 @@
+
 def factorial(n):
     f=1
     for i in range(1,n+1):
@@ -6,6 +7,7 @@ def factorial(n):
 num=int(input("Enter an integer number"))
 #calling function
 print("The factorial of",num,"is",factorial(num))
+
 
 
 
