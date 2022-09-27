3c394db24f1de6bd33c21f5c6adaaddaf2b0d6d9
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2022-09-27 23:00:02 +0530

    Update Reverse_string.py

--- Logicalprobs/Reverse_string.py
+++ Logicalprobs/Reverse_string.py
@@ -3,6 +3,23 @@ txt = "Hello World"[::-1]
 print(txt)
 #O/P -- dlroW olleH
 
+# Function to reverse a string
+def reverseString(s):
+
+   reversedString = ""
+
+   for char in s:
+
+       reversedString = char + reversedString
+
+   return reversedString
+
+s = str(input("Enter a string:"))
+print("The reversed string is - ", reverseString(s))
+
+#O/P --
+
+
 #You can return a range of characters by using the slice syntax.
 #Specify the start index and the end index, separated by a colon, to return a part of the string.
 
