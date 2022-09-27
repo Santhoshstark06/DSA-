d9ac614f8fc4265917193e58c6997394b706b2da
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2022-09-27 22:00:10 +0530

    Update Reverse_string.py

--- Logicalprobs/Reverse_string.py
+++ Logicalprobs/Reverse_string.py
@@ -1,1 +1,17 @@
 
+#You can return a range of characters by using the slice syntax.
+#Specify the start index and the end index, separated by a colon, to return a part of the string.
+
+b = "Hello, World!"
+print(b[2:5])
+
+#0/p -- llo
+
+b = "Hello, World!"
+print(b[:5])
+#O/P -- Hello
+
+b = "Hello, World!"
+print(b[2:])
+
+#O/P -- llo,world
